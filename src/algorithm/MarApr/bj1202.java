01a4972cf08e0bf07bc28874dda643b0622ecc0c
KimGaYeong <rk28716@naver.com>
2022-03-31 10:13:55 +0900

    ..

--- src/algorithm/MarApr/bj1202.java
+++ src/algorithm/MarApr/bj1202.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class bj1202 {
     public static void main(String[] args) throws IOException {
@@ -14,43 +12,52 @@ public class bj1202 {
         System.setIn(input);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        //가방, 보석을 정렬한 뒤 DP를 사용한다면?
+        //가방, 보석을 우선 정렬함
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        Jew[] jews = new Jew[N];
+        ArrayList<Jew> jews = new ArrayList<>();
         Bag[] bags = new Bag[K];
 
+        //보석 입력
         for(int n=0;n<N;n++){
             st = new StringTokenizer(br.readLine());
             int m = Integer.parseInt(st.nextToken());
             int u = Integer.parseInt(st.nextToken());
-            jews[n] = new Jew(m, u);
+            jews.add(new Jew(m, u));
         }
-        Arrays.sort(jews);
+        //보석을 가격 우선, 무게 나중순으로 정렬
+        Collections.sort(jews);
         //System.out.println(Arrays.toString(jews));
+
+        //가방 입력
         for(int k=0;k<K;k++){
             int w = Integer.parseInt(br.readLine());
             bags[k] = new Bag(w);
         }
         Arrays.sort(bags);
-
+        //System.out.println(Arrays.toString(bags));
         long answer=0;
         int idx=0;
+
+        //보석 빼는 기준은 가격 기준.
         PriorityQueue<Jew> pq =new PriorityQueue<>((o1, o2) -> Integer.compare(o2.price,o1.price));
 
         for(int i=0;i<K;i++){
-            //System.out.println("bag[" + i + "]'s limit is " + bags[i].limit);
+            //System.out.println("* bag[" + i + "]'s limit is " + bags[i].limit);
             while (idx<N){
-                //System.out.println("----> jews[" + idx + "].weight is " + jews[idx].weight);
-                if(jews[idx].weight<=bags[i].limit){
-                    pq.add(jews[idx]);
+                //System.out.println("----> jews[" + idx + "].weight is " +jews.get(idx).weight);
+                if(jews.get(idx).weight<=bags[i].limit){
+                    pq.offer(jews.get(idx++));
+                }else{
+                    break;
                 }
-                idx++;
-
             }
+
+            //System.out.println(pq);
             if(!pq.isEmpty()){
                 answer += pq.poll().price;
+
             }
         }
 
@@ -66,7 +73,14 @@ public class bj1202 {
 
         @Override
         public int compareTo(Bag o) {
-            return o.limit;
+            return this.limit-o.limit;
+        }
+
+        @Override
+        public String toString() {
+            return "Bag{" +
+                    "limit=" + limit +
+                    '}';
         }
     }
 
@@ -81,8 +95,7 @@ public class bj1202 {
         @Override
         public int compareTo(Jew o) {
 
-            if(this.price==o.price)return o.weight-this.weight;
-            else return o.price-this.price;
+            return this.weight-o.weight;
 
         }
 
---
+++ src/algorithm/MarApr/bj12865.java
@@ -0,0 +1,68 @@
+package algorithm.MarApr;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj12865 {
+    public static void main(String[] args) throws IOException {
+        InputStream input = bj12865.class.getResourceAsStream("input.txt");
+        System.setIn(input);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        //입력받음
+        Thing[] things = new Thing[N];
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            things[i] = new Thing(w,v);
+        }
+
+        //가지고 있는 배낭 개수 별 최대 가치를 저장할 배열
+        /**
+         * 무게가 0일때, 1일때, 2일때, ... k일때까지
+         * 1번 item만 넣었을 경우, 2번 item까지 넣었을 경우, ~ n번 item까지 넣었을 경우 순서로 저장.
+         */
+        int[][] dp = new int[N][K+1];
+
+        //물건 개수만큼 돌면서
+        for(int i=0; i<N; i++) {
+            //해당 물건의 무게
+            int weight = things[i].w;
+            for(int j=0; j<=K; j++) {
+                if(i==0) {
+                    //물건 개수가 0개일 때는 채운다. (이때도 물건 무게 조건 달아줘야 함)
+                    //물건의 무게가 j보다 작을 때만 넣어준다.
+                    if(weight<=j) dp[i][j] = things[i].v;
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                    if(weight<=j) {
+                        //dp[i-1][j-things[i].w]+things[i].v : j만큼의 무게를 뺀 뒤 현재 물건을 추가로 넣었을 때의 무게와,
+                        //dp[i-1][j] : 안넢었을 때 무게를 비교한다.
+                        dp[i][j] = Math.max(dp[i-1][j-things[i].w]+things[i].v, dp[i-1][j]);
+                    }
+                }
+            }
+        }
+        System.out.println(dp[N-1][K]);
+
+    }
+    //W : 무게 ,V :가치
+
+    public static class Thing{
+        int w, v;
+
+        public Thing(int w, int v) {
+            this.w = w;
+            this.v = v;
+        }
+    }
+}
